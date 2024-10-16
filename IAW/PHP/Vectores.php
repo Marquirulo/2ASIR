c85ad9e8a9760c152495f67dfb1c76e7c4f9dedf
Marquirulo <mcosgarciagonzalez@gmail.com>
2024-10-16 10:35:37 +0200

    Update and rename Vectores.md to Vectores.php

---
+++ IAW/PHP/Vectores.php
@@ -0,0 +1,47 @@
+Arrays
+$temp [0]=22.5 ----->  22,5 es "posicion" 0
+$temp [1]=17.5 ----->  17.5 es "posicion" 1
+$temp [2]=30   ----->  30   es "posicion" 2
+...
+echo $temp[1] --> 17.5
+
+count($temp)=3 // Cuenta los registros que tiene el vector
+
+for (i=0;i< count($temp);i++)
+
+$edades = array('a','b','c')
+
+
+
+<?php
+// Esto lo que hace es modificar lo sindices del array o nombres
+  $registro['dni'] = "20456322";
+  $registro['nombre'] = "Martinez Pablo";
+  $registro['direccion'] = "Colon 134";
+  echo $registro['nombre'];
+?>
+<?php
+  // Otra manera es 
+  $registro = array('dni'=>'20456322',
+                  'nombre'=>'Martinez Pablo',
+                  'direccion'=>'Colon 134');
+  echo $registro['dni'];
+?>
+<?php
+// Para recorrerlo
+  foreach($registro as $clave=>$valor)
+    echo "El array con indice $clave tiene el valor $valor";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
