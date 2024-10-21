f1188491a2296d9a55f16f1e7593864cee8657cd
Marquirulo <mcosgarciagonzalez@gmail.com>
2024-10-21 10:54:45 +0200

    Update BasesDeDatos.php

--- IAW/PHP/BasesDeDatos.php
+++ IAW/PHP/BasesDeDatos.php
@@ -1,12 +1,32 @@
 3 pasos
-1.Apertura de la conexion y seleccion de la BBDD
-$conexionPHP=mysqli_connect (maquina,usuario,contaseña,nombre basedatos)
-2.Operaciones en BBDD
-mysql_query($conexionPHP, consulta).mysqli_error($conexionPHP)
-3.CERRAR conexion
+<?php
+//1.Apertura de la conexion y seleccion de la BBDD
+$conexionPHP=mysqli_connect (maquina,usuario,contraseña,nombre basedatos)
+
+//2.Operaciones en BBDD
+
+$sql="insert into alumnos(nombre,mail,codigoclase) values ('$_REQUEST[nombre]',...)"  
+  
+mysql_query($conexionPHP, $sql).mysqli_error($conexionPHP)
+
+
+
+//Se ponen los values entre comillas para que al insertar lo inserte como VARCHAR como en oracle / si no es texto no va entre comillas
+
+//3.CERRAR conexion
 mysqli_Close($conexionPHP)
 
+// MYSQL TIENE QUE ESTAR EJECUTANDOSE EN XAMP
+?>
 
+localhost/phpmyadmin 
+
+Creo la base de datos y añado la tabla como en oracle
+
+Create table alumnos (  nombre VARCHAR(20),
+                        mail   VARCHAR(20),
+                        CodigoCurso DECIMAL(10,2)
+)
 
 
 
